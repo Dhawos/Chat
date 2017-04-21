f2102539411706a388c5279d3d17575b32c2ba69
Louis Gérard <louis.gerard@isen.yncrea.fr>
2017-04-21 16:04:45 -0400

    initial commit

---
+++ Message.py
@@ -0,0 +1,5 @@
+class Message:
+    def __init__(self,channel,message):
+        self.channel = channel
+        self.message = message
+
---
+++ client/Client.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+class Client():
+    def __init__(self):
+        pass
+
+    def connect(self,host,port):
+        pass
+
+client = Client()
+client.connect("127.0.0.1","8080")
+
+
